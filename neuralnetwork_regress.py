fc220846ee5090b3c7a84602e66eb8de533b1166
Leonam Alves <leon14alv@gmail.com>
2024-03-22 21:27:26 -0300

    document updated

--- neuralnetwork_regress.py
+++ neuralnetwork_regress.py
@@ -9,29 +9,54 @@ def preprocessing(file):
     return X_train, x_test, y_train, y_test
 
 
-def rede_neural(X_train, y_train):
+def rede_neural(X_train, y_train, X_test):
     import pandas as pd
     from keras.models import Sequential 
     from keras.layers import Dense, Dropout 
         
     model = Sequential()
     model.add(Dense(units=3, activation='relu', input_shape =(4,)))
-    model.add(Dense(units=20, activation='relu'))
-    model.add(Dropout(0.1))
+    model.add(Dense(units=500, activation='relu'))
+    model.add(Dropout(0.12))
     model.add(Dense(units=1, activation='relu'))
     model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
     model.summary()
-    model.fit(X_train, y_train, epochs=50, batch_size=10, verbose=True, validation_data=(X_train, y_train))
+    model.fit(X_train, y_train, epochs=300, batch_size=10, verbose=True, validation_data=(X_train, y_train))
     nova_prev = model.predict(X_train)
     prev = pd.DataFrame(nova_prev)
-    return prev
+    
+    prev_test = model.predict(X_test)
+    prev_test = pd.DataFrame(prev_test)
+    
+    return prev, model, prev_test
 
 
-def grafico(prev):
+def grafico_treino(prev, y_train, prev_test, y_test):
     import matplotlib.pyplot as plt
     
     plt.figure(figsize=(10,5))
-    plt.plot(prev)
+    plt.plot(y_train[:30], color='Blue', label='Treinamento')
+    plt.plot(prev[:30], color='Orange', label='Previsão do Treinamento')
+    plt.legend(loc='upper left', fontsize= 10)
     plt.xlabel('Período')
     plt.ylabel('Valor')
     plt.show()
+    
+    
+    plt.figure(figsize=(10,5))
+    plt.plot(y_test[:30], color='Red', label='Teste')
+    plt.plot(prev_test[:30], color='Green', label='Previsão do Teste')
+    plt.legend(loc='upper left', fontsize= 10)
+    plt.xlabel('Período')
+    plt.ylabel('Valor')
+    plt.show()
+    
+    
+file = 'advertising.csv'
+
+X_train, x_test, y_train, y_test = preprocessing(file) #Processamento e Feature engineering
+prev, model, prev_test = rede_neural(X_train, y_train, x_test) #Construção da Rede e previsão
+print(prev)
+
+graph = grafico_treino(prev, y_train, prev_test, y_test) #Gráficos
+print(graph)
